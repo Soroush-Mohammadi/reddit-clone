cc5eb2aed2ff863535e3e634cb45b0e077194c6c
Soroush-Mohammadi <mohamadysoroush@gmail.com>
2025-09-18 17:22:27 +0330

    AppHeader component with related icons complete

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -1,4 +1,19 @@
 <template>
-  <nav>navbar</nav>
-  <Icon name="uil:github" size="3em" />
+  <nav class="bg-richBlack flex items-center justify-between p-1">
+    <Icon name="ant-design:menu-outlined" style="color: white" size="1.5rem" />
+    <img src="/Reddit-logo.png" alt="" class="w-10" />
+    <button class="bg-persimmon rounded-3xl w-20 h-6 text-white">login</button>
+    <icon
+      name="ant-design:search-outlined"
+      style="color: white"
+      size="1.5rem"
+    />
+    <button>
+      <icon
+        name="ant-design:ellipsis-outlined"
+        style="color: white"
+        size="2rem"
+      />
+    </button>
+  </nav>
 </template>
