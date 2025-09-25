38f03eef827ed04753227e9cde767d649386203b
Soroush-Mohammadi <mohamadysoroush@gmail.com>
2025-09-25 17:42:17 +0330

    posts connect to the database

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -8,18 +8,20 @@ const posts = fakePosts;
 </script>
 
 <template>
-  <div class="lg:grid lg:grid-cols-12 gap-8 lg:gap-2  bg-richBlack">
+  <div class="lg:grid lg:grid-cols-12 gap-8 lg:gap-2 bg-richBlack">
     <div
-      class="hidden col-span-2 xl:block flex flex-col border-t scrollbar-hide hover:scrollbar-auto border-r text-white font-light overflow-auto"
+      class="hidden col-span-2 xl:block flex-col border-t scrollbar-hide hover:scrollbar-auto border-r text-white font-light overflow-auto"
     >
       <MobileMenu />
     </div>
     <div
-      class="col-start-4 col-span-6 lg:col-start-1 lg:col-span-9 xl:col-start-4  xl:col-span-6 flex flex-col p-2 border border-gray-500 rounded-lg"
+      class="col-start-4 col-span-6 lg:col-start-1 lg:col-span-9 xl:col-start-4 xl:col-span-6 flex flex-col p-2 border border-gray-500 rounded-lg"
     >
       <PostItem v-for="post in fakePosts" :key="post.id" :post="post" />
     </div>
-    <div class="col-start-10 col-span-2 lg:col-start-10 lg:col-span-3 xl:col-span-2">
+    <div
+      class="col-start-10 col-span-2 lg:col-start-10 lg:col-span-3 xl:col-span-2"
+    >
       <CommunityMenu />
     </div>
   </div>
--- app/pages/posts.vue
+++ app/pages/posts.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-const { data: posts, error, pending } = await useFetch('/api/posts')
+const { data: posts, error, pending } = await useFetch('/api/posts');
 </script>
 
 <template>
-  <div class="text-white">
+  <div class="bg-white">
     <h1>Posts</h1>
     <div v-if="pending">Loading...</div>
     <div v-else-if="error">Error loading posts</div>
