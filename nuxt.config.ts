a09ef6cd54de59ab6312ea7217422a3aee7e6352
soroush <mohamadysoroush@gmail.com>
2025-09-28 03:42:04 +0330

    migration for users and some auth configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,11 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/fonts', '@nuxt/icon']
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/fonts', '@nuxt/icon', '@sidebase/nuxt-auth'],
+  auth: {
+    isEnabled: true,
+    origin: process.env.NEXTAUTH_URL, // e.g. http://localhost:3000
+    basePath: '/api/auth',
+    enableGlobalAppMiddleware: true
+  }
 });
